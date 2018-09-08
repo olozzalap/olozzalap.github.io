3144518cb7c650420f64cd0b8ec6a3af6f8a46ae
Eben Palazzolo <olozzalap@gmail.com>
2018-09-08 19:09:14 -0400

    Added custom section linking via jquery.scrollTo

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ $(document).ready( function () {
 	if ($(document).width() > 950) {
 		$.jInvertScroll(['.oloz-main', '.oloz-scroll-bg']);
 	}
+
+	$('.oloz-home-link').click( () => {
+		$.scrollTo('0', 220);
+	})
+	$('.oloz-about-link').click( () => {
+		$.scrollTo('50%', 220);
+	})
+	$('.oloz-work-link').click( () => {
+		$.scrollTo('max', 220);
+	})
 })
